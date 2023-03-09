d543ee039733ebf0b01ed120c3d8ba25cd647ff2
Abhishek Anand <abhishek.anand941995@gmail.com>
2023-03-09 10:49:50 +0530

    Create test_v1.py
    Trial commit 1

---
+++ test_v1.py
@@ -0,0 +1,33 @@
+
+# ## to activate virtual env
+# .venv\Scripts\activate
+
+# ## to download pandas
+# pip install pandas 
+
+# Testing the file contents
+print("123") 
+import pandas as pd
+import matplotlib
+
+a = pd.DataFrame(columns=['a','b','c'])
+a
+
+
+# # Don't use with Anaconda distributions because they include matplotlib already.
+
+# # macOS
+# python3 -m pip install matplotlib
+
+# # Windows (may require elevation)
+# py -m pip install matplotlib
+
+# # Linux (Debian)
+# apt-get install python3-tk
+# python3 -m pip install matplotlib
+
+
+# ## ctrl+shift+enter to run in new terminal
+# ## shift+enter to run in new terminal
+
+
